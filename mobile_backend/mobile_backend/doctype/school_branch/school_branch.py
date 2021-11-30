4df1ec0a2f6c14317ba1ec22882d4f63e25803ec
Omar Alhori <omaralhoori@gmail.com>
2021-11-30 10:40:28 +0300

    Add get school data apis

--- mobile_backend/mobile_backend/doctype/school_branch/school_branch.py
+++ mobile_backend/mobile_backend/doctype/school_branch/school_branch.py
@@ -3,8 +3,21 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class SchoolBranch(Document):
 	pass
+
+
+@frappe.whitelist(allow_guest=True)
+def get_branches():
+	return frappe.db.sql("""SELECT name, branch_name FROM `tabSchool Branch`""", as_dict=True)
+
+@frappe.whitelist(allow_guest=True)
+def get_classes():
+	return frappe.db.sql("""SELECT name, class_name FROM `tabSchool Class`""", as_dict=True)
+
+@frappe.whitelist(allow_guest=True)
+def get_sections():
+	return frappe.db.sql("""SELECT name, section_name FROM `tabSchool Section`""", as_dict=True)
