cae96386fbee46a9b3b7110493bb9d1f91a0e9f7
omar <omaralhoori@gmail.com>
2022-03-09 22:05:50 +0300

    add privacy policy

---
+++ mobile_backend/www/PrivacyPolicy.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from copy import Error
+import frappe
+from frappe import _
+import requests
+from xml.etree import ElementTree
+
+def get_context(context):
+    return context
---
+++ mobile_backend/www/Text Preview_files/ca-pub-0438674711778385.js
@@ -0,0 +1,1 @@
+(google_async_config = window.google_async_config || {})['ca-pub-0438674711778385'] = {"sra_enabled":false};
