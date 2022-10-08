ca8caf36ffcd439512caef17b03780bb95e48e6f
Omar <omaralhoori@gmail.com>
2022-10-08 17:24:59 +0200

    fix degree reprot url

--- mobile_backend/mobile_backend/degree.py
+++ mobile_backend/mobile_backend/degree.py
@@ -7,7 +7,7 @@ URL = 'http://194.165.141.2:8888/reports/rwservlet?report=DGR02STDPE_RET&userid=
 
 @frappe.whitelist(allow_guest=True)
 def get_report():
-    settings = frappe.get_single("School Settings")
+    #settings = frappe.get_single("School Settings")
 
     #report = frappe.form_dict.report
     
@@ -23,7 +23,7 @@ def get_report():
     report = "DGR02STDA_MN2" if int(semester) == 9 else "DGR02STD_MN2"
     print(all_periods, period, semester, report)
     #url = f"{settings.data_url}/reports/rwservlet?report={report}&userid={settings.user_id}&PERCENFLG=1&STDTRNFLG=1&CONCATMATFLG=2&CONCATMATFLG_DTL=0&SUMPERDIPFLG=2&STDFLG=2&PYEAR={year}&PALLPER={all_periods}&PCLASS={class_no}&PDIV={division_no}&PBRN={branch_no}&PPER={period}&PSEM={semester}&STDBRN={branch_no}&STDCON={contract_no}&STDNO={student_no}"
-    url = f"{settings.data_url}/reports/rwservlet?report={report}&userid={settings.user_id}&PERCENFLG=1&STDTRNFLG=1&CONCATMATFLG=2&CONCATMATFLG_DTL=0&SUMPERDIPFLG=2&STDFLG=2&PYEAR={year}&PALLPER={all_periods}&PCLASS={class_no}&PDIV={division_no}&PBRN={branch_no}&PPER={period}&PSEM={semester}&STDBRN={branch_no}&STDCON={contract_no}&STDNO={student_no}"
+    url = f"http://46.185.139.178:7778/reports/rwservlet?report={report}&userid=MOBUSR/M0B_2O20_Y5N@manar&DESFORMAT=PDF&DESTYPE=Cache&PERCENFLG=1&STDTRNFLG=1&CONCATMATFLG=2&CONCATMATFLG_DTL=0&SUMPERDIPFLG=2&STDFLG=2&PYEAR={year}&PALLPER={all_periods}&PCLASS={class_no}&PDIV={division_no}&PBRN={branch_no}&PPER={period}&PSEM={semester}&STDBRN={branch_no}&STDCON={contract_no}&STDNO={student_no}"
     print(url)
     res = requests.get(url)
     frappe.local.response.filename = "Degree Report"
