756a62a65d06cf368f6ba4bd3dabd9da94dd2aa2
omar <omaralhoori@gmail.com>
2022-10-08 16:39:26 +0300

    bind with oracle

--- mobile_backend/mobile_backend/degree.py
+++ mobile_backend/mobile_backend/degree.py
@@ -9,7 +9,8 @@ URL = 'http://194.165.141.2:8888/reports/rwservlet?report=DGR02STDPE_RET&userid=
 def get_report():
     settings = frappe.get_single("School Settings")
 
-    report = frappe.form_dict.report
+    #report = frappe.form_dict.report
+    
     year = frappe.form_dict.year
     all_periods = frappe.form_dict.all_periods
     class_no = frappe.form_dict.class_no
@@ -19,9 +20,11 @@ def get_report():
     semester= frappe.form_dict.semester
     contract_no = frappe.form_dict.contract_no
     student_no = frappe.form_dict.student_no
-
+    report = "DGR02STDA_MN2" if int(semester) == 9 else "DGR02STD_MN2"
+    print(all_periods, period, semester, report)
+    #url = f"{settings.data_url}/reports/rwservlet?report={report}&userid={settings.user_id}&PERCENFLG=1&STDTRNFLG=1&CONCATMATFLG=2&CONCATMATFLG_DTL=0&SUMPERDIPFLG=2&STDFLG=2&PYEAR={year}&PALLPER={all_periods}&PCLASS={class_no}&PDIV={division_no}&PBRN={branch_no}&PPER={period}&PSEM={semester}&STDBRN={branch_no}&STDCON={contract_no}&STDNO={student_no}"
     url = f"{settings.data_url}/reports/rwservlet?report={report}&userid={settings.user_id}&PERCENFLG=1&STDTRNFLG=1&CONCATMATFLG=2&CONCATMATFLG_DTL=0&SUMPERDIPFLG=2&STDFLG=2&PYEAR={year}&PALLPER={all_periods}&PCLASS={class_no}&PDIV={division_no}&PBRN={branch_no}&PPER={period}&PSEM={semester}&STDBRN={branch_no}&STDCON={contract_no}&STDNO={student_no}"
-    #print(url)
+    print(url)
     res = requests.get(url)
     frappe.local.response.filename = "Degree Report"
     frappe.local.response.filecontent = res.content #get_pdf(html)
