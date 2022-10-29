16a7083b81eeb03d9ccc0dff8dfc3a6afcec8e88
omar <omaralhoori@gmail.com>
2022-10-29 21:28:24 +0300

    degree merge

--- mobile_backend/mobile_backend/degree.py
+++ mobile_backend/mobile_backend/degree.py
@@ -23,9 +23,26 @@ def get_report():
     report = "DGR02STDA_MN2" if int(semester) == 9 else "DGR02STD_MN2"
     # print(all_periods, period, semester, report)
     #url = f"{settings.data_url}/reports/rwservlet?report={report}&userid={settings.user_id}&PERCENFLG=1&STDTRNFLG=1&CONCATMATFLG=2&CONCATMATFLG_DTL=0&SUMPERDIPFLG=2&STDFLG=2&PYEAR={year}&PALLPER={all_periods}&PCLASS={class_no}&PDIV={division_no}&PBRN={branch_no}&PPER={period}&PSEM={semester}&STDBRN={branch_no}&STDCON={contract_no}&STDNO={student_no}"
-    url = f"http://46.185.139.178:7778/reports/rwservlet?report={report}&userid=MOBUSR/M0B_2O20_Y5N@manar&DESFORMAT=PDF&DESTYPE=Cache&PERCENFLG=1&STDTRNFLG=1&CONCATMATFLG=2&CONCATMATFLG_DTL=0&SUMPERDIPFLG=2&STDFLG=2&PYEAR={year}&PALLPER={all_periods}&PCLASS={class_no}&PDIV={division_no}&PBRN={branch_no}&PPER={period}&PSEM={semester}&STDBRN={branch_no}&STDCON={contract_no}&STDNO={student_no}"
+    options = frappe.db.sql(f"""
+        SELECT hide_percentage, merge_subjects, merge_subjects_with_details, hide_total FROM `tabSchool Class`
+        WHERE name='{class_no}'
+    """, as_dict=True)
+    hide_percentage, merge_subjects, merge_subjects_with_details, hide_total= 1, 2, 0, 2
+    if len(options) > 0:
+        hide_percentage = get_results_options('hide_percentage', options[0]['hide_percentage'])
+        merge_subjects = get_results_options('merge_subjects', options[0]['merge_subjects'])
+        merge_subjects_with_details = get_results_options('merge_subjects_with_details', options[0]['merge_subjects_with_details'])
+        hide_total = get_results_options('hide_total', options[0]['hide_total'])
+
+    url = f"http://46.185.139.178:7778/reports/rwservlet?report={report}&userid=MOBUSR/M0B_2O20_Y5N@manar&DESFORMAT=PDF&DESTYPE=Cache&PERCENFLG={hide_percentage}&STDTRNFLG=1&CONCATMATFLG={merge_subjects}&CONCATMATFLG_DTL={merge_subjects_with_details}&SUMPERDIPFLG={hide_total}&STDFLG=2&PYEAR={year}&PALLPER={all_periods}&PCLASS={class_no}&PDIV={division_no}&PBRN={branch_no}&PPER={period}&PSEM={semester}&STDBRN={branch_no}&STDCON={contract_no}&STDNO={student_no}"
     # print(url)
     res = requests.get(url)
     frappe.local.response.filename = "Degree Report"
     frappe.local.response.filecontent = res.content #get_pdf(html)
     frappe.local.response.type = "pdf"
+
+def get_results_options(option_name, option):
+    if option_name == 'merge_subjects_with_details':
+        return 1 if option else 0
+    else:
+        return 1 if option else 2
